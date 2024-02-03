385ccf40ec8510c4dc64148983fc4435f484044c
kev-69 <bismarkobuobi19@gmail.com>
2024-02-03 12:20:32 +0000

    A new nav bar component

---
+++ react-router-budget/src/Components/Navbar.jsx
@@ -0,0 +1,40 @@
+// React Router Dom imports
+import { Form, NavLink } from "react-router-dom";
+
+// Library imports for heroicons
+import { TrashIcon } from '@heroicons/react/24/solid'
+
+// Nav bar assets imports
+import logomark from "../assets/logomark.svg";
+
+
+const Navbar = ({ userName }) => {
+  return (
+    <nav>
+        <NavLink to="/" aria-label="Go to home">
+            <img src={logomark} alt="" height={30} />
+            <span>Home Budget</span>
+        </NavLink>
+        {
+            //userName && (
+                <Form 
+                    method="post" 
+                    action="/logout"
+                    onSubmit={(event) => {
+                        if (!confirm("Delete user and all data?")) {
+                        event.preventDefault()
+                        }
+                    }}>
+                        
+                    <button type="submit" className="btn btn--warning">
+                        <span>Delete User</span>
+                    <TrashIcon width={20}/>
+                    </button> 
+                </Form>
+            
+        }
+    </nav>
+  )
+}
+
+export default Navbar
