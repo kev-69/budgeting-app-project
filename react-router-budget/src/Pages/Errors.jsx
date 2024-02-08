2907b26069f42db96730698ff98c5ef947ec56f2
kev-69 <bismarkobuobi19@gmail.com>
2024-02-08 13:56:41 +0000

    Added a new error message

--- react-router-budget/src/Pages/Errors.jsx
+++ react-router-budget/src/Pages/Errors.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+//React Router Dom imports
+import { Link, useNavigate, useRouteError } from 'react-router-dom'
+
+//Heroicon library imports
+import { HomeIcon, ArrowUturnLeftIcon } from '@heroicons/react/24/solid'
+
 const Errors = () => {
+    const error = useRouteError ();
+    const navigate = useNavigate ();
+
   return (
-    <div>Error</div>
+    <div className="error">
+        <h2>Sorry, we've got a problem.</h2>
+        <p>{error.message || error.statusText}</p>
+
+        <div className="flex-md">
+            <button className="btn btn--dark" onClick={()=> navigate(-1)}>
+                <span>Go Back</span>
+                <ArrowUturnLeftIcon width={20}/>
+            </button>
+            <Link to="/" className="btn btn--dark" >
+                <span>Go Home</span>
+                <HomeIcon width={20}/>
+            </Link>
+        </div>
+    </div>
   )
 }
 
