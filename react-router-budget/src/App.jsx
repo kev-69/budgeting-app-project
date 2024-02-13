ffcd21a9dbddb9bf0845a8026d9f32fe484cc4b6
kev-69 <bismarkobuobi19@gmail.com>
2024-02-13 22:37:33 +0000

    Added the paths from new pages and components

--- react-router-budget/src/App.jsx
+++ react-router-budget/src/App.jsx
@@ -1,7 +1,5 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// React Router Dom imports
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
 
 //Library imports
 import { ToastContainer } from 'react-toastify';
@@ -11,11 +9,14 @@ import 'react-toastify/dist/ReactToastify.css';
 import Main, { mainLoader} from "./Layouts/Main";
 
 // Routes
-import Dashboard, { dashboardAction, dashboardLoader } from "./Pages/Dashboard";
+import Dashboard, {dashboardAction, dashboardLoader} from "./Pages/Dashboard";
 import Errors from "./Pages/Errors";
+import ExpensePage, {expensesLoader, expenseAction} from "./Pages/ExpensePage";
+import BudgetPage, {budgetLoader, budgetAction} from "./Pages/BudgetPage";
 
 // Actions
 import { logoutAction } from "./Actions/logout";
+import { deleteBudget } from "./Actions/deleteBudget";
 
 const router = createBrowserRouter([
   { 
@@ -29,11 +30,32 @@ const router = createBrowserRouter([
         element: <Dashboard />,
         loader: dashboardLoader,
         action: dashboardAction,
+        errorElement: <Errors />,
+      },
+      {
+        path: "budget/:id",
+        element: <BudgetPage />,
+        loader: budgetLoader,
+        action: budgetAction,
+        errorElement: <Errors />,
+        children: [
+          {
+            path: "delete",
+            action: deleteBudget,
+
+          },
+        ]
+      },
+      {
+        path: "expenses",
+        element: <ExpensePage />,
+        loader: expensesLoader,
+        action: expenseAction,
         errorElement: <Errors />
       },
       {
         path: "logout",
-        action: logoutAction 
+        action: logoutAction,
       }
     ]
   },
